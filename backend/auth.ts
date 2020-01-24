d07ce1c5621128ecd317765cd83d0b1020063d60
Guilherme Albuquerque <guilhermealbuquerquerc@gmail.com>
2020-01-24 17:53:28 -0300

    iMeat Aplication

---
+++ backend/auth.ts
@@ -0,0 +1,4 @@
+
+export const handleAuthentication = (req, res) => {
+
+}
--- backend/server.ts
+++ backend/server.ts
@@ -1,5 +1,10 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
+import * as jsonServer from 'json-server'
+import {Express} from 'express'
+import * as fs from 'fs'
+import * as https from 'https'
+
+
+const server: Express = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
@@ -11,9 +16,17 @@ server.use(middlewares)
 // You can use the one used by JSON Server
 server.use(jsonServer.bodyParser)
 
-
+server.post('/login', (req,res) => {
+  res.json({nome:"success"})
+})
 // Use default router
 server.use(router)
-server.listen(3000, () => {
+
+const options = {
+  cert: fs.readFileSync("./backend/keys/cert.pem"),
+  key: fs.readFileSync("./backend/keys/key.pem")
+}
+
+https.createServer(options, server).listen(3001 , () => {
   console.log('JSON Server is running')
 })
