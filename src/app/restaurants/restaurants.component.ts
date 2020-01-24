3498b194ee092f58355aae804512543940fe0426
Guilherme Albuquerque <guilhermealbuquerquerc@gmail.com>
2020-01-24 16:21:02 -0300

    iMeat Aplication

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -1,20 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Restaurant } from './restaurant/restaurant.model';
 import { RestaurantsService } from './restaurants.service';
+import { trigger, state, animate, style, transition } from '@angular/animations';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+
+
+import 'rxjs/add/operator/switchMap'
+import 'rxjs/add/operator/do'
+import 'rxjs/add/operator/debounceTime'
+import 'rxjs/add/operator/distinctUntilChanged'
 
 @Component({
   selector: 'mt-restaurants',
   templateUrl: './restaurants.component.html',
+  animations: [
+    trigger('toggleSearch', [
+      state('hidden', style({
+        opacity: 0,
+        "max-height": "0px"
+      })),
+      state('visible', style({
+        opacity: 1,
+        "max-height": "70px",
+        "margin-top": "20px"
+      })),
+      transition('* => *', animate('200ms ease-in-out'))
+    ])
+  ]
 })
 export class RestaurantsComponent implements OnInit {
-  
+
+  searchBarState = 'hidden'
   restaurants: Restaurant[]
 
-  constructor(private restaurantsService: RestaurantsService) { }
+  searchForm: FormGroup
+  searchControl: FormControl
 
+  constructor(private restaurantsService: RestaurantsService, private fb: FormBuilder) { }
+  @ViewChild('iptSearch') nameField: ElementRef;
   ngOnInit() {
+    this.searchControl = this.fb.control('')
+    this.searchForm = this.fb.group({
+      searchControl: this.searchControl
+    })
+
+    this.searchControl.valueChanges
+      .debounceTime(500)
+      .distinctUntilChanged()
+      .do(searchTerm => console.log(`q=${searchTerm}`))
+      .switchMap(SearchTerm => this.restaurantsService.restaurants(SearchTerm))
+      .subscribe(restaurants => this.restaurants = restaurants)
+
     this.restaurantsService.restaurants()
-    .subscribe(restaurants => this.restaurants = restaurants)
+      .subscribe(restaurants => this.restaurants = restaurants)
   }
 
+  toggleSearch() {
+    this.searchBarState = this.searchBarState === 'hidden' ? 'visible' : 'hidden'
+    this.nameField.nativeElement.focus()
+  }
 }
